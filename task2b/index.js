e55677b90709a3bb7dda388e73f307e275de429e
School Of Code <isabellaRebecca@live.co.uk>
2022-12-15 16:38:33 +0000

    task 2b complete

--- task2b/index.js
+++ task2b/index.js
@@ -1,54 +1,57 @@
+//Plan
+// first of all look through the code to get an understanding of what is happening and expected
+//Run code to see what functions or areas in the code isn't working 
+
+
+//removed line 6 which converted numb1 into a string
+//rectified the typo on line 0
 function add(numb1, numb2) {
-  let num1 = String(numb1);
-  return num1 + numb2;
+  return numb1 + numb2;
 }
 
 function multiply(num1, operator, num2) {
-  num1, operator, num2;
+ return num1 * num2;
 }
 
 function hasPets(hasPet, firstName, lastName) {
-  if (hasPet === "true") {
+  if (hasPet === true) {
     return firstName + " " + lastName + " really does have a nice pet";
   } else {
-    return;
-    firstName + " " + lastName + " " + "has no pets";
+    return firstName + " " + lastName + " " + "has no pets";
   }
 }
 
 let myAccountNumbers = {
-  account1: "220",
+  account1: 220,
   account2: 144,
   account3: 12,
   account4: 1443,
 };
 
 function addsBalanceOfAccounts() {
-  let firstAccount = myAccountNumbers[1];
+  let firstAccount = myAccountNumbers['account2'];
   let secondAccount = myAccountNumbers["account3"];
   return firstAccount + secondAccount;
 }
 
 function calculator(firstNumber, operator, secondNumber) {
   if (operator === "+") {
-    let answer = firstNumber + secondNumber;
-    return;
+    return firstNumber + secondNumber;
+    
   }
   if (operator === "-") {
-    let answer = firstNumber - secondNumber;
-    return;
+    return firstNumber - secondNumber;
+    
   }
   if (operator === "*") {
-    let answer = firstNumber * secondNumber;
-    return;
+    return  firstNumber * secondNumber;
+    
   }
   if (operator === "/") {
-    let answer = firstNumber / secondNumber;
-    return;
+    return firstNumber / secondNumber;
+    
   } else {
-    let answer = "Invalid inputs. Try again!";
-    return;
-    answer;
+    return "Invalid inputs. Try again!";
   }
 }
 
